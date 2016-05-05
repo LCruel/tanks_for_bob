f1b2eff6d148148afb6745a65bcb5bd42af40019
LCruel <dvda_l@hotmail.com>
2016-05-05 22:23:09 +0200

    Fixes
    - Tank recall teleport now works for T2 and Supertanks

--- control.lua
+++ control.lua
@@ -65,22 +65,12 @@ function tank_talk(player_index)
   if( player.character ) then -- must be exist
     if( player.character.health > 0.0 ) then -- must be alive
       if( player.driving == false ) then
-        player.print("Please come back")
         tankCloseGui(player)
       else
         tanktype = player.vehicle.name
-        if ( tanktype == "auto-tank" or tanktype == "auto-tank-wlsk" ) then
-          count_auto_cannon_shells = player.vehicle.get_item_count("45mm-auto")
-          count_cannon_shells = player.vehicle.get_item_count("cannon-shell-2")
-          count_mortar_shells = player.vehicle.get_item_count("50mm-mortar")          
-          player.print("Tank status:")
-          player.print("Auto cannon shells: ".. count_auto_cannon_shells)
-          player.print("Battle cannon shells: ".. count_cannon_shells)
-          player.print("Mortar shells: ".. count_mortar_shells)
+        if ( (tanktype == "flame-tank-wlsk" or tanktype == "auto-tank-wlsk" or tanktype == "super-tank" or tanktype == "super-tank-alternate") and player.force.technologies["tank-recall"].researched ) then
+          player.print("Recal teleporter ready")
           tankOpenGui(player)
-        end
-        if ( tanktype == "flame-tank" or tanktype == "flame-tank-wlsk" ) then
-          player.print("Lets make the sun shine brighter then ever!")
         end
         if ( tanktype == "diesel-locomotive" ) then
           player.print("Hello sir, please be seated in the most lethal vehicle in this game!")
