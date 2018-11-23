441755ca43d30844c9f84cde927db65fe0990735
LCruel <you@example.com>
2018-11-23 21:15:34 +0100

    Re oderding folders

---
+++ tanks_for_bob_1.6.0/prototypes/item/turret.lua
@@ -0,0 +1,14 @@
+data:extend(
+{
+  {
+    type = "item",
+    name = "auto-cannon-turret",
+    icon = "__base__/graphics/icons/gun-turret.png",
+    flags = {"goes-to-quickbar"},
+    subgroup = "defensive-structure",
+    order = "a[turret]-a[turret]",
+    place_result = "auto-cannon-turret",
+    stack_size = 10
+  },
+}
+)
